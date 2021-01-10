d71c4e0b2ca34be853f16e44553d1eb796b12b4a
Roberto Gomez <RobertoGo@protonmail.com>
2021-01-10 17:10:39 +0100

    Final Project - AuthModule and child module setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { Routes, RouterModule } from "@angular/router";
 
-import { Store } from 'store';
+import { Store } from "store";
 
 // feature modules
 
 // containers
-import { AppComponent } from './containers/app/app.component';
+import { AppComponent } from "./containers/app/app.component";
 
 // components
 
@@ -15,18 +15,34 @@ import { AppComponent } from './containers/app/app.component';
 export const ROUTES: Routes = [];
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(ROUTES)
-  ],
-  declarations: [
-    AppComponent
-  ],
-  providers: [
-    Store
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+  imports: [BrowserModule, RouterModule.forRoot(ROUTES)],
+  declarations: [AppComponent],
+  providers: [Store],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-analytics.js"></script>
+
+// <script>
+//   // Your web app's Firebase configuration
+//   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyDtT9JOUR6_0zpZJhwh2c4KbQSnVP3WOsQ",
+//     authDomain: "fitness-app-c1a45.firebaseapp.com",
+//     databaseURL: "https://fitness-app-c1a45-default-rtdb.europe-west1.firebasedatabase.app",
+//     projectId: "fitness-app-c1a45",
+//     storageBucket: "fitness-app-c1a45.appspot.com",
+//     messagingSenderId: "65117212478",
+//     appId: "1:65117212478:web:6b9757ec1ea38f51e5a66f",
+//     measurementId: "G-6V1E4LSR5V"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+// </script>
