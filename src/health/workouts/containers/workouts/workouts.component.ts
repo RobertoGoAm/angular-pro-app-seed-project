2305027440036719a08c9babe7aa0289de3748df
Roberto Gomez <RobertoGo@protonmail.com>
2021-01-16 17:17:52 +0100

    Final Project - HealthModule setup and lazy loading

---
+++ src/health/workouts/containers/workouts/workouts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "workouts",
+  styleUrls: ["workouts.component.scss"],
+  template: ` <div>workouts</div> `
+})
+export class WorkoutsComponent {
+  constructor() {}
+}
