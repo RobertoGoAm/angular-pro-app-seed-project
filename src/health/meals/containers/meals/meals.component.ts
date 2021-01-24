822f617db2afc3a2d2035fb642b77335ddafbafb
Roberto Gomez <RobertoGo@protonmail.com>
2021-01-24 15:26:46 +0100

    Final Project - Async Pipe "as" syntax and routing

--- src/health/meals/containers/meals/meals.component.ts
+++ src/health/meals/containers/meals/meals.component.ts
@@ -11,7 +11,36 @@ import {
 @Component({
   selector: "meals",
   styleUrls: ["meals.component.scss"],
-  template: ` <div>{{ meals$ | async | json }}</div> `
+  template: `
+    <div class="meals">
+      <div class="meals__title">
+        <h1>
+          <img src="/img/food.svg" />
+          Your meals
+        </h1>
+
+        <a class="btn__add" [routerLink]="['../meals/new']">
+          <img src="/img/add-white.svg" />
+          New meal
+        </a>
+
+        <div *ngIf="meals$ | async as meals; else loading">
+          <div class="message" *ngIf="!meals.length">
+            <img src="/img/face.svg" />
+            No meals, add a new meal to start
+          </div>
+          <!-- meals ngFor -->
+        </div>
+
+        <ng-template #loading>
+          <div class="message">
+            <img src="/img/loading.svg" />
+            Fetching meals...
+          </div>
+        </ng-template>
+      </div>
+    </div>
+  `
 })
 export class MealsComponent implements OnInit, OnDestroy {
   meals$: Observable<Meal[]>;
