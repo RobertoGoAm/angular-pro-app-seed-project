5e179830dc279c3d24229ed9a2a39146b0d7ee28
Roberto Gomez <RobertoGo@protonmail.com>
2021-02-22 09:46:37 +0100

    Final Project - Calendar date toggling

--- src/health/schedule/components/schedule-calendar/schedule-calendar.component.ts
+++ src/health/schedule/components/schedule-calendar/schedule-calendar.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output
+} from "@angular/core";
 
 @Component({
   selector: "schedule-calendar",
@@ -9,18 +15,35 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
         [selected]="selectedDay"
         (move)="onChange($event)"
       ></schedule-controls>
+
+      <schedule-days
+        [selected]="selectedDayIndex"
+        (select)="selectDay($event)"
+      ></schedule-days>
     </div>
   `
 })
-export class ScheduleCalendarComponent {
-  @Output() change = new EventEmitter<Date>();
+export class ScheduleCalendarComponent implements OnChanges {
+  selectDayIndex: number;
   selectedDay: Date;
-
+  selectedWeek: Date;
+  @Output() change = new EventEmitter<Date>();
   @Input() set date(date: Date) {
     this.selectedDay = new Date(date.getTime());
   }
 
   constructor() {}
+
+  ngOnChanges() {
+    this.selectDayIndex = this.getToday(this.selectedDay);
+    this.selectedWeek = this.getStartOfWeek(new Date(this.selectedDay));
+  }
+
+  selectDay(index: number) {
+    const selectedDay = new Date(this.selectedWeek);
+    selectedDay.setDate(selectedDay.getDate() + index);
+    this.change.emit(selectedDay);
+  }
 
   onChange(weekOffset: number) {
     const startOfWeek = this.getStartOfWeek(new Date());
@@ -32,6 +55,16 @@ export class ScheduleCalendarComponent {
 
     startDate.setDate(startDate.getDate() + weekOffset * 7);
     this.change.emit(startDate);
+  }
+
+  private getToday(date: Date) {
+    let today = date.getDay() - 1;
+
+    if (today < 0) {
+      today = 6;
+    }
+
+    return today;
   }
 
   private getStartOfWeek(date: Date) {
--- src/health/schedule/components/schedule-days/schedule-days.component.ts
+++ src/health/schedule/components/schedule-days/schedule-days.component.ts
@@ -1,10 +1,36 @@
-import { Component } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output
+} from "@angular/core";
 
 @Component({
   selector: "schedule-days",
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ["schedule-days.component.scss"],
-  template: ` <div class="days"></div> `
+  template: `
+    <div class="days">
+      <button
+        type="button"
+        class="day"
+        *ngFor="let day of days; index as i"
+        (click)="selectDay(i)"
+      >
+        <span [class.active]="i === selected">
+          {{ day }}
+        </span>
+      </button>
+    </div>
+  `
 })
 export class ScheduleDaysComponent {
-  constructor() {}
+  @Input() selected: number;
+  @Output() select = new EventEmitter<number>();
+  days = ["M", "T", "W", "T", "F", "S", "S"];
+
+  selectDay(index: number) {
+    this.select.emit(index);
+  }
 }
